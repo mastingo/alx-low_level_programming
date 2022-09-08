e4227a31c67d8fe45c285a23c9c41a5afb6b5600
mastingo <mativokyle@gmail.com>
2022-09-08 12:28:11 -0700

    4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+* main_ entry point
+*
+* Return: Always 0 (success)
+*/
+int main(void)
+{ puts("Programming is like building a multilingual puzzle\n");
+       	return (0); 
+}

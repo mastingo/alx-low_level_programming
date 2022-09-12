bdc1471955dc213d5fc5d740a434539e367fb2b2
mastingo <mativokyle@gmail.com>
2022-09-12 11:52:58 -0700

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *main - entry point
+ *
+ *Return: 0
+ */
+int main(void)
+{
+	char alphabet = 'a';
+
+	while (alphabet <= 'z')
+
+	{
+		putchar(alphabet);
+		alphabet++;
+	}
+	putchar('\n');
+	return (0);
+}

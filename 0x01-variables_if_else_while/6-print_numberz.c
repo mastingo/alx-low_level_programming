f08c74386b6d83a24339f2cd90eea7b4c70eeeb8
mastingo <mativokyle@gmail.com>
2022-09-12 13:02:26 -0700

    7

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *  main - entry point
+ *  Description - Prints the letters z to a followed by a new line
+ *  Return: 0
+ */
+
+int main(void)
+{
+	char x;
+
+	for (x = 'z'; x >= 'a'; x--)
+	{
+		putchar(x);
+
+	}
+
+	putchar('\n');
+	
+	return (0);
+}

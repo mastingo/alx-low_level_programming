9b8dd4227f3b1435732c619e0cf0e08272cee053
mastingo <mativokyle@gmail.com>
2022-09-12 12:02:29 -0700

    3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description - Prints the alphabets in lowercase and uppercase consecutively
+ * Return: 0
+ */
+
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		putchar(alphabet);
+
+	}
+	putchar('\n');
+
+	return (0);
+}
+

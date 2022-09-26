9a5d142a7fc57fb5426dae5a4d319c64598a6064
mastingo <mativokyle@gmail.com>
2022-09-26 14:37:40 -0700

    0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -15,7 +15,7 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int index;
 
-	for (index = 0; index < n; index++;)
-	*(s + index) = b;
+	for (index = 0; index < n; index++)	
+		*(s + index) = b;
 	return (s);
 }

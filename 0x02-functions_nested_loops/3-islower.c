08bb15d90bfe8b507726536ad9ad492901a2cbc6
mastingo <mativokyle@gmail.com>
2022-10-13 00:34:17 -0700

    3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+  * _islower - check the code
+  * @c: parameter
+  * Description - Checks for lowercase characters
+  *
+  * Return: 0
+  */
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+}

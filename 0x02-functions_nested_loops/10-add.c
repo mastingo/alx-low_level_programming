b673addc595159272a875f87d9eafae44686ffca
mastingo <mativokyle@gmail.com>
2022-10-13 00:47:42 -0700

    13

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - finds the summation of the two arguments
+ *
+ * @a: first parameter
+ * @b: second parameter
+ *
+ * Return: 0
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
